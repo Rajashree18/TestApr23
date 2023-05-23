35090d55cdfab0c9e21ca96d820af43f73c5f923
Rajashree18 <rajashreepatil609@gmail.com>
2023-05-23 08:12:40 +0530

    committed changes in a1 for creating conflict

--- src/a/File1.java
+++ src/a/File1.java
@@ -3,8 +3,9 @@ package a;
 public class File1 {
 
 	public static void main(String[] args) {
-		System.out.println("I am in a branch");
-		System.out.println("branch a changes");
+		System.out.println("I am in main method of branh A1");
+		System.out.println("branch a changes-creating conflict scenario");
+		System.out.println("branch b changes");
 
 	}
 
