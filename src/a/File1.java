b8c7ee58fe05639364be3a16e2be150c7722eab2
Rajashree18 <rajashreepatil609@gmail.com>
2023-05-23 08:59:15 +0530

    branch b chnages

--- src/a/File1.java
+++ src/a/File1.java
@@ -3,8 +3,8 @@ package a;
 public class File1 {
 
 	public static void main(String[] args) {
-		System.out.println("I am in a branch");
-		System.out.println("branch a changes");
+		System.out.println("I am in B branch");
+		System.out.println("branch a changes....");
 
 	}
 
