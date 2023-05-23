60c769457de743a4099e8afe87864eec98b15628
Rajashree18 <rajashreepatil609@gmail.com>
2023-05-23 08:39:32 +0530

    committing chnages after resolving the conflict

--- src/a/File1.java
+++ src/a/File1.java
@@ -6,7 +6,7 @@ public class File1 {
 		System.out.println("I am in main method of branh A1");
 		System.out.println("branch a changes-creating conflict scenario");
 		System.out.println("branch b changes");
-
+		System.out.println("I am in a branch B1");
+		System.out.println("i am in main method");
 	}
-
 }
